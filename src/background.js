9d8014e810d769668f3508cede519f7685522279
Eilon Lipton <Eilon@users.noreply.github.com>
2022-04-08 14:23:38 -0700

    Better error handling and repo lookup for prediction web request

--- src/background.js
+++ src/background.js
@@ -1,14 +1,50 @@
+function getPredictionUrl(predictionRequest) {
+
+  var repoMappings =
+  {
+    "dotnet/aspnetcore": "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/aspnetcore/{0}",
+    "dotnet/maui": "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/maui/{0}"
+  };
+
+  var urlPattern = repoMappings[`${predictionRequest.owner}/${predictionRequest.repo}`];
+
+  if (!urlPattern) {
+    return null;
+  }
+
+  var predictorUrl = urlPattern.replace('{0}', predictionRequest.number);
+
+  return predictorUrl;
+}
+
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
 
     if (request.msg === "getpredictions") {
-      var predictorUrl = `https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/${request.owner}/${request.repo}/${request.number}`;
-      try {
-        fetch(predictorUrl).then(r => r.json().then(j => sendResponse(j)));
-      }
-      catch (e) {
 
+      var predictorUrl = getPredictionUrl({
+        "owner": request.owner,
+        "repo": request.repo,
+        "number": request.number
+      });
+
+      if (!predictorUrl) {
+        sendResponse({ "error": "Unsupported repo" });
+        return;
       }
+
+      fetch(predictorUrl)
+        .then(r => {
+          if (!r.ok) {
+            throw { "error": r.status };
+          }
+          return r;
+        })
+        .then(r => r.json())
+        .then(j => sendResponse(j))
+        .catch(error => {
+          sendResponse(error);
+        });
 
       return true; // Will respond asynchronously.
     };
